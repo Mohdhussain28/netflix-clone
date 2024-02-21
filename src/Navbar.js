9b02eece01e57f1ceab2fe3a17b3caf470960412
Mohdhussain28 <mohdhussain28@outlook.com>
2024-02-22 00:34:18 +0530

    change netflix logo url

--- src/Navbar.js
+++ src/Navbar.js
@@ -18,7 +18,7 @@ function Navbar() {
     return (
         <div className={`nav ${show && "nav_black"}`}>
             <img className='nav_logo'
-                src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png'
+                src='https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png'
                 alt="Netflix Logo" />
 
             <img className='profile_logo'
